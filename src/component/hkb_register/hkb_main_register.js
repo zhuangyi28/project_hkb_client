62601e1eb0afa6bda971841e86ceab35252b933e
zhuangyi28 <596363901@qq.com>
2017-09-06 16:21:48 +0800

    新增注册成功0906

--- src/component/hkb_register/hkb_main_register.js
+++ src/component/hkb_register/hkb_main_register.js
@@ -3,27 +3,31 @@
  */
 /*倒计时*/
 
-    var InterValObj;
-    var nums = 60;
-    var btn;
+     var InterValObj;
+     var nums = 60;//设置秒
+     var btn;//点击按钮
+     var curCount;//当前剩余秒数
+
     function sendCode(thisBtn) {
+
+        curCount = nums;
         btn = thisBtn;
         btn.disabled = true; //将按钮置为不可点击
-        btn.value = '重新发送' + nums + 's';
+        btn.value = '重新发送' + curCount + 's';
         InterValObj = setInterval(doLoop, 1000); //一秒执行一次
     }
 
     function doLoop() {
 
-        if (nums == 0) {
+        if (curCount == 0) {
             window.clearInterval(InterValObj); //清除js定时器
             btn.disabled = false;
             btn.style.borderColor = '#f8c166';
             btn.value = '获取验证码';
 
         } else {
-            nums--;
-            btn.value = '重新发送' + nums + 's';
+            curCount--;
+            btn.value = '重新发送' + curCount + 's';
         }
     };
 
